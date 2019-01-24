c3ee3ab402eaa63ee38bb11055e5bf3de0273668
devel <p.kelbing96@web.de>
2019-01-24 09:49:48 +0100

    documentation of client connection

--- client_connection.py
+++ client_connection.py
@@ -4,11 +4,21 @@ import time
 
 
 class ClientConnection(Thread):
+    """
+    ClientConnection represents a socket connection to one client/player
+    """
 
     # message   : Class message
     # msg       : Received Data
 
     def __init__(self, server, client_socket):
+        """
+        Creates a instance of ClientConnection with a reference to the server and the client socket.
+        :param server: Server
+        :type server: Server
+        :param client_socket: Socket of the client
+        :type client_socket: socket
+        """
         Thread.__init__(self)
         self.server = server
         self.client_socket = client_socket
@@ -16,9 +26,22 @@ class ClientConnection(Thread):
         self.start()
 
     def send_message(self, message):
+        """
+        Send a message over a socket to the client. The message is UTF-8 encoded and a binary zero is appended to
+        ensure the split between multiple messages.
+        :param message: Message to be send
+        :type message: Message
+        :return: None
+        """
         self.client_socket.sendall(bytes((str(message)+'\0').encode("utf-8")))
 
     def on_message(self, msg):
+        """
+        This event is called if a new message was received.
+        :param msg: Message (hopefully in JSON-format as string)
+        :type msg: str
+        :return: None
+        """
         message = Message(msg)
         print(message)
         msg_type = message.get_type()
@@ -36,14 +59,29 @@ class ClientConnection(Thread):
             self.on_gamedata()
 
     def on_connect(self, nickname):
+        """
+        This event is called if a connection message was received.
+        :param nickname: Nickname of the connection/player
+        :type nickname: str
+        :return: None
+        """
         self.nickname = nickname
 
     def on_disconnect(self):
+        """
+        This event is called if a disconnect message was received. The socket will be closed and a event another event
+        on the server will be called.
+        :return: None
+        """
         # Close connection
         self.client_socket.close()
         self.server.on_disconnect(self)
 
     def on_ping(self):
+        """
+        This event is called if a ping message was received. A pong message will be send instant.
+        :return: None
+        """
         # Create and send pong message
         message = Message()
         message.set_type(message.TYPE_PING)
@@ -52,15 +90,32 @@ class ClientConnection(Thread):
         self.send_message(message)
 
     def on_timeout(self):
+        """
+        This event will be called if a timeout happens.
+        :return: None
+        """
         pass
 
     def on_gamedata(self):
+        """
+        This event will be called if game date was received.
+        :return: None
+        """
         pass
 
     def on_error(self):
+        """
+        This event will be called if a error happens.
+        :return: None
+        """
         pass
 
     def run(self):
+        """
+        This functions starts a loop which checks if data was received and append data pieces together til a full
+        message was transmitted.
+        :return: None
+        """
         received_data = ""
         while True:
             piece = str(self.client_socket.recv(4096).decode("utf-8"))
